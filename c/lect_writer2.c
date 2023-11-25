3ca495519b58370cb9a21b5ab064da1f6823f37e
Acselr <axel.sneessens@student.uclouvain.be>
2023-11-25 17:13:35 +0100

    3 code avec mes sémaaphore de mort la, OUISTITIIIIIIIIIIIIIIIIII

---
+++ c/lect_writer2.c
@@ -0,0 +1,202 @@
+#include "header/lect_writer.h"
+#include "sem.c"
+
+// Global var
+pthread_t* readers;
+pthread_t* writers;
+
+sem rsem; // Access to 'DB'
+sem wsem;  // max 1 writer at once
+
+pthread_mutex_t mwc; // mutex for write count
+int write_count;
+
+pthread_mutex_t z; 
+pthread_mutex_t mrc; // mutex for read count
+int read_count;
+
+int nb_readers;
+int nb_writers;
+
+
+void read_data() {
+    for (int i=0; i<10000; i++);
+}
+
+void write_data() {
+    for (int i=0; i<10000; i++);
+}
+
+// 2560
+void* reader() {
+    // reader method. Reads carefully while making sure data is 
+    // synchronised / used by multiple threads properly
+     int iter = 2560 / nb_readers;
+
+    for (int i = 0; i < iter; i++) {
+        pthread_mutex_lock(&z);
+        wait(&rsem);
+
+        pthread_mutex_lock(&mrc);
+        read_count++;
+
+        if (read_count == 1) {
+            wait(&wsem);
+        }
+
+        pthread_mutex_unlock(&mrc);
+        post(&rsem);
+        pthread_mutex_unlock(&z);
+
+        read_data();
+
+        pthread_mutex_lock(&mrc);
+        read_count--;
+
+        if (read_count == 0) {
+            post(&wsem);
+        }
+        pthread_mutex_unlock(&mrc);
+    }
+
+    return (void* ) NULL;
+}
+
+// 640 
+void* writer() {
+    // Writer method. Writes carefully while making sure data is 
+    // synchronised / used by multiple threads properly
+    int iter = 640 / nb_writers;
+    for (int i = 0; i < iter; i++) {
+        
+        pthread_mutex_lock(&mwc);
+        // Critical section
+
+        write_count++;
+        if (write_count == 1) {
+            wait(&rsem);
+        }
+        pthread_mutex_unlock(&mwc);
+
+        wait(&wsem);
+        write_data();
+        post(&wsem);
+
+        pthread_mutex_lock(&mwc);
+        // Critical section
+
+        write_count--;
+        if (write_count == 0) {
+            post(&rsem);
+        }
+        pthread_mutex_unlock(&mwc);
+
+    }
+
+    return (void *) NULL;
+}
+
+int main(int argc, const char* argv[]) {
+
+    int err;    
+    
+    // read initialisation:
+    err = init(&rsem, 1);
+    if (err != 0) return -1;
+    
+    read_count = 0;
+    
+    err = pthread_mutex_init(&mrc, NULL); // mutex for read count
+    if (err != 0) return -2;
+    
+    // write initialisation
+    err = init(&wsem, 1);
+    if (err != 0) return -1;
+    
+    write_count = 0;
+    
+    
+    err = pthread_mutex_init(&mwc, NULL); // mutex for write count
+    if (err != 0) return -2;
+    
+    err = pthread_mutex_init(&z, NULL); 
+    if (err != 0) return -2;
+    
+    if (argc != 3) {
+        printf("\nWrong number of arguments, should be: ./lect-writer nb_readers nb_writers\n");
+        return -3;
+    }
+
+    nb_readers = atoi(argv[1]);
+    nb_writers = atoi(argv[2]);
+
+    
+    // create array of threads
+    readers = (pthread_t *) malloc(nb_readers * sizeof(pthread_t));
+    writers = (pthread_t *) malloc(nb_writers * sizeof(pthread_t));
+
+
+    if (readers == NULL || writers == NULL) return -3;
+    
+
+    if (nb_readers == nb_writers) {
+        // create step by step 1 reader and one writer
+        for (int t_i = 0; t_i < nb_readers; t_i++) {
+
+            err = pthread_create(&(writers[t_i]), NULL, &writer, NULL);
+            if (err != 0) return -4;
+            
+            err = pthread_create(&(readers[t_i]), NULL, &reader, NULL);
+            if (err != 0) return -4;
+
+        }  
+    } else {
+        // firstly create all writers and then all readers (not as good)
+        for (int t_i = 0; t_i < nb_writers; t_i++) {
+
+            err = pthread_create(&(writers[t_i]), NULL, &writer, NULL);
+            if (err != 0) return -4;
+        }
+        for (int t_i = 0; t_i < nb_readers; t_i++) {
+
+            err = pthread_create(&(readers[t_i]), NULL, &reader, NULL);
+            if (err != 0) return -4;
+        } 
+    }
+
+    
+    // Join threads
+    for (int t_i = 0; t_i < nb_writers; t_i++) {
+
+        err = pthread_join(writers[t_i], NULL);
+        if (err != 0) return -5;
+    }
+    for (int t_i = 0; t_i < nb_readers; t_i++) {
+
+        err = pthread_join(readers[t_i], NULL);
+        if (err != 0) return -5;
+    }
+
+    // free malloc (threads lists)
+    free(readers);
+    free(writers);
+
+    // destroy semaphores
+    err = destroy(&rsem);
+    if (err != 0) return -6;
+
+    err = destroy(&wsem);
+    if (err != 0) return -6;
+
+    // destroy mutex
+    err = pthread_mutex_destroy(&mwc);
+    if (err != 0) return -7;
+
+    err = pthread_mutex_destroy(&mrc);
+    if (err != 0) return -7;
+
+    err = pthread_mutex_destroy(&z);
+    if (err != 0) return -7;
+
+    return 0;
+}
---
+++ c/prod_conso2.c
@@ -0,0 +1,151 @@
+#include "header/prod_conso.h"
+#include "sem.c"
+
+#define N 8 // places dans le buffer
+
+int buffer[N];
+pthread_mutex_t mutex;
+sem empty;
+sem full;
+
+int* IdProd;
+int* IdCons;
+
+pthread_t* prod; // liste des producteur (threads)
+pthread_t* cons; // liste des consomateur (threads)
+
+int todo = 8192;  // nbr d'item a faire
+int toeat = 8192;  // nbr d'item a manger
+
+int index_place_libre = 0;
+
+int randomInt(){
+    int INT_MAX= 2147483647;
+    int INT_MIN = -2147483648;
+
+    long long range = (long long)INT_MAX - (long long)INT_MIN + 1;
+    int randomNumber = (int)(((long long)rand() * range) / (INT_MAX + 1) + INT_MIN);
+
+    return randomNumber;
+}
+
+void insert_item(int item){
+    buffer[index_place_libre] = item;
+    index_place_libre++;
+    for (int i=0; i<10000; i++);  // simule utilisation du cpu
+}
+
+void delete_item(){
+    index_place_libre--;
+    for (int i=0; i<10000; i++);  // simule utilisation du cpu
+}
+
+void* producer(void* id) {
+    int item;
+    while (1) {
+        item = randomInt();
+        wait(&empty);
+        pthread_mutex_lock(&mutex);
+
+        if (todo <= 0) {
+            pthread_mutex_unlock(&mutex);
+            post(&full);
+            break;
+        }
+
+        //printf("thread[%d]: add\n", *(int*)id);
+        insert_item(item);
+        todo--;
+
+        pthread_mutex_unlock(&mutex);
+        post(&full);
+    }
+    return NULL;
+}
+
+void* consumer(void* id) {
+    while (1) {
+        wait(&full);
+        pthread_mutex_lock(&mutex);
+
+        if (toeat <= 0) {
+            pthread_mutex_unlock(&mutex);
+            post(&empty);
+            break;
+        }
+
+        //printf("thread[%d]: supp\n", *(int*)id);
+        delete_item();
+        toeat--;
+
+        pthread_mutex_unlock(&mutex);
+        post(&empty);
+    }
+    return NULL;
+}
+
+int main(int argc, const char* argv[]) {  // argv[1] = nombre de prod, argv[2] = nombre de cons; 
+    pthread_mutex_init(&mutex, NULL);
+    init(&empty, N);
+    init(&full, 0);
+    int NP;
+    int NC;
+    if (argc == 3){
+        NP = atoi(argv[1]);  // define number of prod
+        NC = atoi(argv[2]);  // define number of cuns
+    }
+    else return -1; 
+
+    // allocates memory for prod/cons, idProd/idCons
+    prod = (pthread_t *) malloc(NP * sizeof(pthread_t));
+    cons = (pthread_t *) malloc(NC * sizeof(pthread_t));
+    IdProd = (int *) malloc(NP * sizeof(int));
+    IdCons = (int *) malloc(NC * sizeof(int));
+    
+    int err;
+    for (size_t i = 0; i < NP; i++){
+        IdProd[i] = i;
+        err = pthread_create(&(prod[i]), NULL, &producer, &(IdProd[i]));  // init the threads (philosopher)
+        if(err!=0) {
+            printf("Error: %d", -2);
+            return -2;
+        }
+        IdCons[i] = i;
+        err = pthread_create(&(cons[i]), NULL, &consumer, &(IdCons[i]));  // init the threads (philosopher)
+        if(err!=0) {
+            printf("Error: %d\n", -3);
+            return -3;
+        }
+    }
+
+    for (int i=0; i< NP; i++) {
+        err = pthread_join(prod[i], NULL);  // wait threads (philosopher)
+        if(err!=0) {
+            printf("Error: %d\n", -4);
+            return -4;
+        }
+
+        err = pthread_join(cons[i], NULL);  // wait threads (philosopher)
+        if(err!=0) {
+            printf("Error: %d\n", -5);
+            return -5;
+        }
+    }
+
+
+    err = pthread_mutex_destroy(&(mutex));
+    if (err != 0) {
+        printf("Error: %d\n", err);
+        return -6;
+    }
+
+    destroy(&full);
+    destroy(&empty);
+
+    free(prod);
+    free(cons);
+    free(IdProd);
+    free(IdCons);
+
+    return 0;
+}
